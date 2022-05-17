a0c415ac89e00d35759b80cdbb1ef23affb831b5
Joel <joeldome17@gmail.com>
2022-05-16 22:49:04 -0500

    Proecto Final - Interfaz del Login

---
+++ kata_javascript/Proyecto-CajeroAutomatico/js/db.js
@@ -0,0 +1,17 @@
+const cuentas = [
+  {
+    id_cliente: 1,
+    nombre: "Mali", 
+    saldo: 20, 
+  },
+  {
+    id_cliente: 2,
+    nombre: "Gera",
+    saldo: 290,
+  },
+  {
+    id_cliente: 3,
+    nombre: "Maui",
+    saldo: 67,
+  },
+];
