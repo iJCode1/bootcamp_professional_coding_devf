7231318ef92c5badcdc272b66dc556ca077ee80d
Joel <joeldome17@gmail.com>
2022-05-17 00:16:06 -0500

    Proyecto Final - Funcionalidad de login lista

---
+++ kata_javascript/Proyecto-CajeroAutomatico/js/app.js
@@ -0,0 +1,48 @@
+// *************** Declaración de variables ***************
+const $inputEmail = document.querySelector('#inputEmail');
+const $inputPassword = document.querySelector('#inputPassword');
+const $inputEmailMessage = document.querySelector('#inputEmailMessage');
+const $inputPasswordMessage = document.querySelector('#inputPasswordMessage');
+const $buttonEnter = document.querySelector('#buttonEnter');
+let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+
+// *************** Declaración de funciones ***************
+const validateAccount = (data, email, pass) =>{
+  if(!(emailRegex.test(email))){
+    return messageError("El correo electrónico que se ingresó no es válido", $inputEmailMessage);
+  }else if(pass.length < 6){
+    clearMessageError($inputEmailMessage);
+    return messageError("La contraseña debe ser mayor o igual a 6 dígitos", $inputPasswordMessage);
+  }else{
+    clearMessageError($inputEmailMessage);
+    clearMessageError($inputPasswordMessage);
+    
+    let findEmail = data.filter((el) => el.correo === email);
+    if(findEmail.length === 1){
+      if( ( findEmail[0].correo === email ) && ( findEmail[0].contraseña === pass ) ){
+        window.location.href = './cajero.html';
+      }else{
+        return messageError("La contraseña no coincide con el correo ingresado, verifíquelo por favor", $inputPasswordMessage);
+      }
+    }else{
+      return messageError("Revise el correo ingresado, no se encuentra en la BD", $inputEmailMessage);
+    }
+  }
+}
+
+const messageError = (error, element)=>{
+  element.textContent = error;
+  element.style.visibility = 'initial';
+}
+
+const clearMessageError = (element) =>{
+  element.textContent = '';
+}
+
+// *************** Declaración de eventos ***************
+$buttonEnter.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let email = $inputEmail.value;
+  let pass = $inputPassword.value.trim();
+  validateAccount(cuentas, email, pass);
+});
--- kata_javascript/Proyecto-CajeroAutomatico/js/db.js
+++ kata_javascript/Proyecto-CajeroAutomatico/js/db.js
@@ -1,17 +1,23 @@
 const cuentas = [
   {
     id_cliente: 1,
-    nombre: "Mali", 
+    nombre: "Mali",
+    correo: 'mali1969@gamil.com',
+    contraseña: 'Gatito123',
     saldo: 20, 
   },
   {
     id_cliente: 2,
     nombre: "Gera",
+    correo: 'gera.marquez12@hotmail.com',
+    contraseña: 'SoyUnGera09',
     saldo: 290,
   },
   {
     id_cliente: 3,
     nombre: "Maui",
+    correo: 'maui.cortez90@gmail.com',
+    contraseña: '01_mauiCortez',
     saldo: 67,
   },
 ];
