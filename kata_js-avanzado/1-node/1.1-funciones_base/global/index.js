ad6a5d1ebbaec55208dcf73bdf3135d1e68fe624
Joel <joeldome17@gmail.com>
2022-07-05 21:09:30 -0500

    Primeros pasos con Node

---
+++ kata_js-avanzado/1-node/1.1-funciones_base/global/index.js
@@ -0,0 +1,6 @@
+console.log("Ruta donde se encuentra el archivo",__dirname);
+console.log("Ruta donde se encuentra el archivo + nombre",__filename);
+
+// export: Para exportar modulos (archivos).
+// module: Referencia al módulo actual
+// Require(): Importa un archivo js como modulo. require('index.js');
