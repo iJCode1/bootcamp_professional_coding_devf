8d1e6ca6638046408b04e6a46c56b415017402ca
Joel <joeldome17@gmail.com>
2022-07-06 19:23:40 -0500

    chore: Actualización de archivos de la carpeta 1.1 - Funciones Base

--- kata_js-avanzado/1-node/1.1-funciones_base/global/index.js
+++
@@ -1,6 +0,0 @@
-console.log("Ruta donde se encuentra el archivo",__dirname);
-console.log("Ruta donde se encuentra el archivo + nombre",__filename);
-
-// export: Para exportar modulos (archivos).
-// module: Referencia al módulo actual
-// Require(): Importa un archivo js como modulo. require('index.js');
