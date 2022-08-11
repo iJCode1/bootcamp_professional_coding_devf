c1ae19a4bc8d94086612de50b6db1ca78ea1a6fa
Joel <joeldome17@gmail.com>
2022-08-10 20:21:34 -0500

    feat: comandos básicos de yarn

---
+++ kata_frontend/yarn-project/comandosYarn.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * Instalar paquetes
+ * 
+ * install - yarn add package_name
+ * 
+ * install dev dependencies - yardn add --dev package_name
+ * 
+ * uninstall package - yarn remove package_name
+ * 
+ * update - yarn upgrade
+ */
