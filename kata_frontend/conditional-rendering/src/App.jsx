07e246c6775bc183ca756786770242fc7c3d9c14
Joel <joeldome17@gmail.com>
2022-08-18 19:48:17 -0500

    Conditional rendering en React

---
+++ kata_frontend/conditional-rendering/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Counter from './Counter';
+import UserActive from './UserActive';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [age, setAge] = useState(22);
+  const [isActive, setIsActive] = useState(true);
+
+  const showGreeting = (age) => {
+    const message = (age >= 18) ? <span>Hola, eres mayor de edad 😎</span> : <span>No eres mayor de edad 😕</span>;
+    return message;
+  };
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      {/* JSX */}
+      {showGreeting(22)}
+
+      {/* Conditional Rendering */}
+      {/* Si solo hay un if, se usa && */}
+      {count >= 0 &&
+        (
+          <Counter/>
+        )
+      }
+
+      {/* Si hay un else-if, se usa operador ternario */}
+      { isActive ? <UserActive/> : <h3>Usuario no activo</h3>}
+
+      {/* Si se requiere, se puede anidar un ternario dentro de otro ternario */}
+      { age >= 18 ? (
+        <strong>Eres mayor de edad</strong>
+        ) : age < 18 && age >= 12 ? (
+        <b>Eres un adolescente</b> 
+        ) : (
+        <h5>Eres un niño</h5>)
+      }
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ kata_frontend/conditional-rendering/src/UserActive.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserActive = () => {
+  return (
+    <h3>Usuario activo</h3>
+  )
+}
+
+export default UserActive
