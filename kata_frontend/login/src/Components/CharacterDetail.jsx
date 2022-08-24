b342adeef17ad00bceb7f45e5f706a5ab2dbc88e
Joel <joeldome17@gmail.com>
2022-08-23 21:33:24 -0500

    feat: Consumiendo la API de Star Wars en el proyecto de login

---
+++ kata_frontend/login/src/Components/CharacterDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterDetail = ({id}) => {
+  return (
+    <div>
+      <h3>ID del personaje seleccionado {id}</h3>
+    </div>
+  )
+}
+
+export default CharacterDetail
--- kata_frontend/login/src/Components/Welcome.jsx
+++ kata_frontend/login/src/Components/Welcome.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import CharacterDetail from './CharacterDetail';
 import './Welcome.css';
+import axios from 'axios';
 
 const Welcome = ( { setLoginStatus } ) => {
+
+  const[charactersAray, setCharactersAray] = useState([]);
+  const [characterID, setCharacterID] = useState(0);
+
+  const handleCharacterID = (id) => setCharacterID(id);
+  /**
+   * Implementar llamada SWAPI cuando se monte el componente, una sola vez
+   */
+  useEffect(()=>{    
+    getStarWarsCharacters();
+  }, [])
+  
+  const getStarWarsCharacters = async() => {
+    try{
+      const {data: {results} } = await axios.get('https://swapi.dev/api/people');
+      setCharactersAray(results);
+      console.log('result', results);
+    }catch(error){
+      console.error('error', error);
+    }
+  };
 
   /**
    * Función para crear mensaje del componente con la fecha y hora actual
@@ -17,6 +40,19 @@ const Welcome = ( { setLoginStatus } ) => {
       <div className="container mt-5 d-flex flex-column justify-content-center align-items-center form-container p-3">
         <h2 className='mb-4'>Bienvenido 😎!</h2>
         <p> { message() } </p>
+
+        {
+        charactersAray.length === 0 
+        ? <h4>Cargando...</h4>
+        : charactersAray.map( (character, index) => (
+            <p className='select' key={index} onClick={() => handleCharacterID(index+1) }>Nombre del personaje <span className='text-primary'>{character.name} - {character.height}</span></p>
+          ))
+        }
+
+        {
+          characterID > 0 && <CharacterDetail id={characterID}/>
+        }
+
         <form onSubmit={ () => setLoginStatus(false) }>
           <button type='submit' className='btn btn-block btn-danger'>Cerrar Sesion</button>
         </form>
